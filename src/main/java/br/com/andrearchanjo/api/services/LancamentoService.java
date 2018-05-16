1040a0accc6ddf4d78bbb9f5f6e0f682e8042ee6
André Archanjo <archanjoandre@yahoo.com.br>
2018-05-16 11:29:44 -0300

    Adicionado Service Lancamento e seus Testes Unitarios

--- src/main/java/br/com/andrearchanjo/api/services/LancamentoService.java
+++ src/main/java/br/com/andrearchanjo/api/services/LancamentoService.java
@@ -1,5 +1,44 @@
 package br.com.andrearchanjo.api.services;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import br.com.andrearchanjo.api.entities.Lancamento;
+
 public interface LancamentoService {
 
+	/**
+	 * Retorna uma lista paginada de lancamentos de um determinado funcionario.
+	 * 
+	 * @param funcionarioId
+	 * @param pageRequest
+	 * @return Page<Lancamento>
+	 */
+	Page<Lancamento> buscarPorFuncionarioId(Long funcionarioId, PageRequest pageRequest);
+	
+	/**
+	 * Retorna um lancamento por Id.
+	 * 
+	 * @param funcionarioId
+	 * @return Optional<Lancamento>
+	 */
+	Optional<Lancamento> buscarPorId(Long funcionarioId);
+	
+	/**
+	 * Persiste um lançamento na base de dados.
+	 * 
+	 * @param lancamento
+	 * @return
+	 */
+	Lancamento persistir(Lancamento lancamento);
+	
+	/**
+	 * Remove um lançamento na base de dados.
+	 * 
+	 * @param id
+	 */
+	void remover(Long id);
+	
 }
